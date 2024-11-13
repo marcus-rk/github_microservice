9c52f2eca07301cd2533c86d2e192dfb14e703e0
marcus_rk <makj0005@stud.kea.dk>
2024-11-13 10:19:24 +0100

    updated dockerfile

--- app.py
+++ app.py
@@ -81,4 +81,4 @@ def get_github_stats():
         return jsonify({"error": str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=PORT)
+    app.run(host='0.0.0.0', port=80)
